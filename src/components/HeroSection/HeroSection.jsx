d302dbfc63fe28e6bd17411853d00100f9f4ec3e
Spotter1091 <adedayolamide.o@gmail.com>
2023-09-29 15:44:38 +0100

    update file

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -3,25 +3,28 @@ import './HeroSection.css'
 export default function HeroSection() {
     return (
         <>
-        <div className='hero-container'>
-            <div className="content-wrapper">
-                <img src="../icons/border-top-left.svg" alt="icon top right" className='icon-right' />
-                <h1 className="hero-title">
-                    Digitalized Management <br /> For EPC Contractors
-                </h1>
-                <img src="../icons/border-bottom-right.svg" alt="icon bottom left" className='icon-left' />
+            <div className='hero-container'>
+                <div className="content-wrapper">
+                    <img src="../icons/border-top-left.svg" alt="icon top right" className='icon-right' />
+                    <h1 className="hero-title">
+                        Digitalized Management <br /> For EPC Contractors
+                    </h1>
+                    <img src="../icons/border-bottom-right.svg" alt="icon bottom left" className='icon-left' />
+                </div>
+                <button className='hero-btn'>Learn more</button>
             </div>
-            <button className='hero-btn'>Learn more</button>
-            
-        </div>
-        <div className="search-bar-container">
-        <div className='search-bar'>
-            <img src="../icons/search-normal.svg" alt="Search icon" />
-            <span>What type of contractor/vendor are you looking for?</span>
-            <span className='vertical-line'></span>
-
-        </div>
-        </div>
+            <div className="search-bar-container">
+                <div className='search-bar'>
+                    <img src="../icons/search-normal.svg" alt="Search icon" className='search-icon' />
+                    <span className='input-content'>What type of contractor/vendor are you looking for?</span>
+                    <span className='vertical-line'></span>
+                    <span className='input-content-two'>
+                        <img src="../icons/location.svg" alt="Location marker" className='location-marker' />
+                        <span className='marker-text'>Location</span>
+                    </span>
+                    <button className='search-btn'>Search</button>
+                </div>
+            </div>
         </>
     )
 }
